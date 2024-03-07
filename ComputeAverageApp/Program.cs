2597951d4faf21fd64a406d6c9758e44fc822a2c
Lawrence (KR) <Lawrence (KR)@DESKTOP-3TJ8QIS>
2024-03-08 00:21:30 +0800

    Add project files.

---
+++ ComputeAverageApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ComputeAverageApp
+{
+    internal class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+
+            Console.WriteLine("Enter 5 grades separated by new line:");
+            int i = 0;
+            for (i = 1; i <= 5; i++)
+            {
+                double number = Convert.ToDouble(Console.ReadLine());
+                sum += number;
+            }
+
+            if (i == 6)
+            {
+                i--;
+            }
+
+            double average = sum / i;
+
+            Console.WriteLine("The average is " + average + " and round off to " + Math.Round(average));
+        }
+    }
+}
